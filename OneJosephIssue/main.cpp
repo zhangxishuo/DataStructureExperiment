ff090a782d85a6ff2bb932b53b187f9a2e5ea565
zhangxishuo <zhangxishuo1998@gmail.com>
2017-11-19 18:00:09 +0800

    功能实现，代码不咋地

--- OneJosephIssue/main.cpp
+++ OneJosephIssue/main.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 class Node {
 public:
+    int num;
     int password;
     Node *next;
 };
@@ -12,9 +13,10 @@ private:
     Node *headPointer;
 public:
     CircularList();
-    void insert(int password);
+    void insert(int num, int password);
     void show();
-    void remove(int map);
+    int remove(int map, int &num);
+    bool null();
 };
 
 CircularList::CircularList() {
@@ -22,10 +24,11 @@ CircularList::CircularList() {
     headPointer->next = NULL;
 }
 
-void CircularList::insert(int password) {
+void CircularList::insert(int num, int password) {
     Node *node, *p;
     node = (Node *) malloc (sizeof(Node));
     node->password = password;
+    node->num = num;
 
     if (headPointer->next == NULL) {
         headPointer->next = node;
@@ -41,22 +44,51 @@ void CircularList::insert(int password) {
 void CircularList::show() {
     Node *p = headPointer->next;
     for (; p->next != headPointer->next; p = p->next) {
-        cout << p->password;
+        cout << p->num << " " << p->password << endl;
     }
-    cout << p->password;
+    cout << p->num << " " << p->password << endl;
 }
 
-void CircularList::remove(int map) {
-    Node *p = headPointer;
-    for (int i = 0; i < map - 1; i ++) {
-        p = p->next;
+int CircularList::remove(int map, int &num) {
+    Node *p = headPointer->next;
+    Node *q;
+    int password;
+    if (map == 1) {
+        if (p->next == p) {
+            headPointer->next = NULL;
+        } else {
+            for (; p->next != headPointer->next; p = p->next);
+            q = headPointer->next;
+            headPointer->next = headPointer->next->next;
+            p->next = headPointer->next;
+            num = q->num;
+            password = q->password;
+            free(q);
+        }
+    } else {
+        for (int i = 0; i < map - 2; i ++) {
+            p = p->next;
+        }
+        Node *delNode = p->next;
+        password = delNode->password;
+        num = delNode->num;
+        p->next = delNode->next;
+        headPointer->next = delNode->next;
+        if (delNode == delNode->next) {
+            headPointer->next = NULL;
+        }
+        free(delNode);
     }
-    Node *q = p->next;
-    p->next = q->next;
-    if (q == q->next) {
-        headPointer->next = NULL;
+
+    return password;
+}
+
+bool CircularList::null() {
+    if (headPointer->next == NULL) {
+        return true;
     }
-    free(q);
+
+    return false;
 }
 
 int main() {
@@ -73,10 +105,24 @@ int main() {
     }
 
     for (i = num - 1; i >= 0; i --) {
-        circularList.insert(password[i]);
+        circularList.insert(i + 1, password[i]);
     }
 
     circularList.show();
+
+    int map, index;
+    cout << "the start map" << endl;
+    cin  >> map;
+
+    while (!circularList.null()) {
+        map = circularList.remove(map, index);
+        cout << index << endl;
+    }
+
+    getchar();
+    getchar();
+    getchar();
+    getchar();
 
     return 0;
 }
