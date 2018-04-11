ee84adbbc2470c608564e23961389dd4c7920b53
zhangxishuo <zhangxishuo1998@gmail.com>
2018-04-11 15:15:25 +0800

    引入zanui模板，完成首页tab

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,66 +1,25 @@
-// pages/index/index.js
-Page({
+const { extend, Tab } = require('../../zanui/index');
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
-  
-  },
+Page(extend({}, Tab, {
+    data: {
+        tabs: {
+            list: [{
+                id: 'inFilm',
+                title: '正在热映'
+            }, {
+                id: 'inCome',
+                title: '即将上映'
+            }],
+            selectedId: 'inFilm'
+        }
+    },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-  
-  },
+    handleZanTabChange(e) {
+        var componentId = e.componentId;
+        var selectedId = e.selectedId;
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
-  }
-})
+        this.setData({
+            [`${componentId}.selectedId`]: selectedId
+        });
+    }
+}));
